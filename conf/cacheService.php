f1ad2dbed48c39034ab8ef14a6d0bd4522a4f08d
gbrtalk <48175724+gbrtalk@users.noreply.github.com>
2019-05-03 10:06:13 +0800

    Add files via upload

---
+++ conf/cacheService.php
@@ -0,0 +1,12 @@
+<?php 
+defined('WEKIT_VERSION') or exit(403);
+
+/**
+ * »º´æ·þÎñÅäÖÃ
+ */
+return array(
+	'PwUser' => 'user.cache.PwUserDbCache',
+	'PwForum' => 'forum.cache.PwForumDbCache',
+	'PwThread' => 'forum.cache.PwThreadDbCache',
+	'forum.dao.PwPostsDao' => 'forum.cache.PwPostDbCache'
+);
